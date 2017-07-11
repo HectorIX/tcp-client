0634db7daaa8c3b1df7f70489ce45aae8c351c3d
HectorIX <giorgoschiotis@protonmail.com>
2017-07-11 19:24:34 +0100

    Sign in completed

--- src/client_sign_in.rs
+++ src/client_sign_in.rs
@@ -1,28 +1,26 @@
+extern crate rpassword;
+
+
 use std::io::{self};
 
 
 pub fn sign_in() -> String {
 
-    let mut username = String::new();
-    let mut password = String::new();
+
     let mut full_request = "sign_up_state::Sign-in**".to_owned();
 
     println!("===============================\n\tSIGN IN\n\n" );
-    println!("username: ");
-    io::stdin().read_line(&mut username).unwrap();
+    let username = rpassword::prompt_response_stdout("username: ").unwrap();
 
-    let req_vector: Vec<&str> = username.split("\n").collect();
-    let no_new_line = req_vector[0].to_string();
 
-    full_request.push_str(&no_new_line);
-
+    full_request.push_str(&username);
     full_request.push_str("--");
 
-    println!("password: ");
-    io::stdin().read_line(&mut password).unwrap();
-
-    println!("");
+    let password = rpassword::prompt_password_stdout("password: ").unwrap();
+    println!("\nPlease press enter...");
     full_request.push_str(&password);
+
+
     full_request.to_string()
 
 }
