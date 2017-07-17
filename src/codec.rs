bad3d4d1e088b6ae51808fd07d1556723d69aa66
HectorIX <giorgoschiotis@protonmail.com>
2017-07-17 17:40:26 +0100

    codec added

---
+++ src/codec.rs
@@ -0,0 +1,112 @@
+use bytes::{BufMut, BytesMut};
+use futures::sync::mpsc;
+use futures::{Sink, Future};
+use tokio_io::codec::{Encoder, Decoder};
+
+
+use std::io::{self, Read};
+use std::str;
+use parser;
+
+
+
+
+
+/// A simple `Codec` implementation that just ships bytes around.
+///
+/// This type is used for "framing" a TCP stream of bytes but it's really just a
+/// convenient method for us to work with streams/sinks for now. This'll just
+/// take any data read and interpret it as a "frame" and conversely just shove
+/// data into the output location without looking at it.
+pub struct Bytes;
+
+impl Decoder for Bytes {
+
+    type Item = BytesMut;
+    type Error = io::Error;
+
+    fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<BytesMut>> {
+
+        if buf.len() > 0 {
+
+            let end = buf.len();
+            let input = buf.split_to(end);
+
+            let response = str::from_utf8(&input).unwrap();
+            let output =  parser::response_decomposer(response.to_string());
+
+            Ok(Some(output))
+
+        } else {
+
+            Ok(None)
+        }
+
+    }
+
+
+    fn decode_eof(&mut self, buf: &mut BytesMut) -> io::Result<Option<BytesMut>> {
+        self.decode(buf)
+    }
+}
+
+impl Encoder for Bytes {
+    type Item = Vec<u8>;
+    type Error = io::Error;
+
+    fn encode(&mut self, data: Vec<u8>, buf: &mut BytesMut) -> io::Result<()> {
+        buf.put(&data[..]);
+        Ok(())
+    }
+}
+
+// Our helper method which will read data from stdin and send it along the
+// sender provided.
+pub fn read_stdin(mut tx: mpsc::Sender<Vec<u8>>) {
+
+
+    let mut stdin = io::stdin();
+
+
+    loop {
+
+        let mut buf = vec![0; 1024];
+
+        let n = match stdin.read(&mut buf) {
+            Err(_) |
+            Ok(0) => break,
+            Ok(n) => n,
+        };
+
+        buf.truncate(n);
+
+        let full_request = translate(buf);
+
+
+
+        if full_request == "exit".to_string().into_bytes() {
+
+            break;
+        }
+        else {
+
+            tx = tx.send(full_request.clone()).wait().unwrap();
+        }
+
+    }
+}
+
+
+
+fn translate(buffer:Vec<u8>) -> Vec<u8> {
+
+
+    let readble_form = match str::from_utf8(&buffer) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    let client_request = parser::request_constructor(readble_form.to_string());
+
+    client_request.to_string().into_bytes()
+}
--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ extern crate tokio_io;
 extern crate bytes;
 
 
-
+mod codec;
 mod menu;
 mod user;
 mod parser;
@@ -31,18 +31,18 @@ mod generate_password;
 mod integrity;
 
 use std::env;
-use std::io::{self, Read, Write};
+use std::io::{self, Write};
 use std::net::SocketAddr;
 use std::thread;
 use std::str;
 
-use bytes::{BufMut, BytesMut};
+
 use futures::sync::mpsc;
-use futures::{Sink, Future, Stream};
+use futures::{ Future, Stream};
 use tokio_core::net::TcpStream;
 use tokio_core::reactor::Core;
 use tokio_io::AsyncRead;
-use tokio_io::codec::{Encoder, Decoder};
+
 
 
 
@@ -70,7 +70,7 @@ fn main() {
     // read data from stdin and then send it to the event loop over a standard
     // futures channel.
     let (stdin_tx, stdin_rx) = mpsc::channel(0);
-    thread::spawn(|| read_stdin(stdin_tx));
+    thread::spawn(|| codec::read_stdin(stdin_tx));
     let stdin_rx = stdin_rx.map_err(|_| panic!()); // errors not possible on rx
 
     // After the TCP connection has been established, we set up our client to
@@ -93,7 +93,7 @@ fn main() {
 
     let client = tcp.and_then(|stream| {
 
-        let (sink, stream) = stream.framed(Bytes).split();
+        let (sink, stream) = stream.framed(codec::Bytes).split();
         let send_stdin = stdin_rx.forward(sink);
         let write_stdout = stream.for_each(move |buf| {
             stdout.write_all(&buf)
@@ -106,104 +106,4 @@ fn main() {
 
     // And now that we've got our client, we execute it in the event loop!
     core.run(client).unwrap();
-}
-
-
-/// A simple `Codec` implementation that just ships bytes around.
-///
-/// This type is used for "framing" a TCP stream of bytes but it's really just a
-/// convenient method for us to work with streams/sinks for now. This'll just
-/// take any data read and interpret it as a "frame" and conversely just shove
-/// data into the output location without looking at it.
-struct Bytes;
-
-impl Decoder for Bytes {
-
-    type Item = BytesMut;
-    type Error = io::Error;
-
-    fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<BytesMut>> {
-
-        if buf.len() > 0 {
-
-            let end = buf.len();
-            let input = buf.split_to(end);
-
-            let response = str::from_utf8(&input).unwrap();
-            let output =  parser::response_decomposer(response.to_string());
-
-            Ok(Some(output))
-
-        } else {
-
-            Ok(None)
-        }
-
-    }
-
-
-    fn decode_eof(&mut self, buf: &mut BytesMut) -> io::Result<Option<BytesMut>> {
-        self.decode(buf)
-    }
-}
-
-impl Encoder for Bytes {
-    type Item = Vec<u8>;
-    type Error = io::Error;
-
-    fn encode(&mut self, data: Vec<u8>, buf: &mut BytesMut) -> io::Result<()> {
-        buf.put(&data[..]);
-        Ok(())
-    }
-}
-
-// Our helper method which will read data from stdin and send it along the
-// sender provided.
-fn read_stdin(mut tx: mpsc::Sender<Vec<u8>>) {
-
-
-    let mut stdin = io::stdin();
-
-
-    loop {
-        
-        let mut buf = vec![0; 1024];
-
-        let n = match stdin.read(&mut buf) {
-            Err(_) |
-            Ok(0) => break,
-            Ok(n) => n,
-        };
-
-        buf.truncate(n);
-
-        let full_request = translate(buf);
-
-
-
-        if full_request == "exit".to_string().into_bytes() {
-
-            break;
-        }
-        else {
-
-            tx = tx.send(full_request.clone()).wait().unwrap();
-        }
-
-    }
-}
-
-
-
-fn translate(buffer:Vec<u8>) -> Vec<u8> {
-
-
-    let readble_form = match str::from_utf8(&buffer) {
-        Ok(v) => v,
-        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-    };
-
-    let client_request = parser::request_constructor(readble_form.to_string());
-
-    client_request.to_string().into_bytes()
 }
